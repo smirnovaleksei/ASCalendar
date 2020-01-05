792a29f5ded7e1e087f747261bf52a73d550b313
smirnovaleksei <29alex291997@mail.ru>
2020-01-05 23:09:36 +0300

    Initial Commit

---
+++ ASCalendar/ASCalendar.h
@@ -0,0 +1,19 @@
+//
+//  ASCalendar.h
+//  ASCalendar
+//
+//  Created by Алексей Смирнов on 05.01.2020.
+//  Copyright © 2020 Алексей Смирнов. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ASCalendar.
+FOUNDATION_EXPORT double ASCalendarVersionNumber;
+
+//! Project version string for ASCalendar.
+FOUNDATION_EXPORT const unsigned char ASCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASCalendar/PublicHeader.h>
+
+
