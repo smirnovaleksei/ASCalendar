3f9346acd3f6a9cd647011f3800396cd0f5be02e
Алексей Смирнов <als@finch-melrose.com>
2020-01-10 12:12:28 +0300

    test

---
+++ ASCalendar/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  ASCalendar
+//
+//  Created by finch on 10.01.2020.
+//  Copyright © 2020 Алексей Смирнов. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    public init() { }
+    
+    public static func doSomething() -> String {
+        return "do something"
+    }
+}
