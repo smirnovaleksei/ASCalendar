9e6f496d0fc566af80abb57acd3d359c13e6e45e
Алексей Смирнов <als@finch-melrose.com>
2020-01-28 13:33:42 +0300

    add Example

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -59,28 +59,3 @@ struct ContentView: View {
     
 }
 
-struct MyCalendarStyle: PrimitiveCalendarCofiguration {
-    
-    var calendar: Calendar = .current
-    
-    var dateFormatter: DateFormatter {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd.MM.yyyy"
-        return dateFormatter
-    }
-    
-    var textColor: Color = .black
-    
-    var cellColor: Color = .white
-    
-    var font: Font = .custom("DIN Alternate", size: 25)
-    
-    var selectedTextColor: Color = .white
-    
-    var selectedCellColor: Color = .red
-    
-    var beetweenTextColor: Color = .black
-    
-    var betweenCellColor: Color = Color.init(UIColor(red: 233/255, green: 240/255, blue: 247/255, alpha: 0.8))
-       
-}
